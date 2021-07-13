46811dfaefd32d7d4b90f646e579f7590b309396
Piotr Koszela <piotr.koszela@gmail.com>
2021-07-13 16:31:40 +0200

    add config

---
+++ public/config.js
@@ -0,0 +1,12 @@
+window.cdh_settings = {
+	"baseUrl": "",
+	"customerID": "",
+	"containerName": "",
+	"channel": "",
+	"data": {
+		"name": "Mary Romano",
+		"avatar": "MaryRomano.jpg",
+		"sitelogo": "uplus.png",
+		"sitecaption": "U+ Comms"
+	}
+}
